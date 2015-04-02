b8138d822b85c42ce107ae39b6fabfabd5f8a474
Geoff Nin <geoff.nin@gmail.com>
2015-04-01 21:34:54 -0700

    added better fake data

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function generateContacts() {
   var data = [];
   for(var i = 0; i < 1000; i++) {
     data.push({
-      name: "test name",
-      avatar: "img src"
+      name: faker.name.findName(),
+      avatar: faker.internet.avatar()
     });
   }
   return data;
